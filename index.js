544e6dea32bd604ef2ce5eb7f2caac936c185cdc
davidknight <davidb_nunes@yahoo.com.br>
2023-03-14 21:55:36 -0500

    Concatenate strings, Interpolate variables and other JS expressions

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = "Shawn Knight";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser},!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
